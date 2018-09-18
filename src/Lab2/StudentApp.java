e7a122819995d268f61209b2d3cc03b564c4da86
p077535 <p077535@FRLH100362.corp.noxiane.net>
2018-09-18 10:59:37 +0200

    commit from eclipse

--- src/Lab2/StudentApp.java
+++ src/Lab2/StudentApp.java
@@ -2,14 +2,15 @@ package Lab2;
 
 public class StudentApp {
 	public static void main(String[] args) {
-		
-		Student s1 = new Student("Douv", "123456789");	
+
+		Student s1 = new Student("Douv", "123456789");
 		System.out.println(s1.toString());
 		System.out.println("Adding code from github");
+	}
 }
 
 class Student {
-	
+
 	private static int iD = 1000;
 	private String userId;
 	private String name;
@@ -32,15 +33,15 @@ class Student {
 		int upper = 9000;
 		int lower = 1000;
 		int r = (int) (Math.random() * (upper - lower)) + lower;
-		System.out.println("random: "+r);
-		userId = iD + "" + r + SSN.substring( SSN.length() - 4 );
+		System.out.println("random: " + r);
+		userId = iD + "" + r + SSN.substring(SSN.length() - 4);
 
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Name : " + name + ",\nUserId : " + userId + ",\nEmail : " + email;
-		
+
 	}
 
 	public String getPhone() {
